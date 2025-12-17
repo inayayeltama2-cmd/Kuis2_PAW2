af17e7f477765e4e33b0d01d2c8b59823ba79375
inayayeltama <inayayeltama2@gmail.com>
2025-12-17 11:58:30 +0700

    loginComponent

---
+++ angular-login-admin/src/app/login.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { AuthService } from './services/auth.service';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+  <div class="container mt-4">
+    <h2>Selamat datang, Admin</h2>
+    <p>Gunakan navigasi untuk mengelola data penduduk.</p>
+  </div>
+  `,
+})
+export class HomeComponent {
+  constructor(private auth: AuthService, private router: Router) {}
+}
