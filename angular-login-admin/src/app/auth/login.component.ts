d9502f4f018830792bd632d70bf4d4a860d39163
inayayeltama <inayayeltama2@gmail.com>
2025-12-17 12:51:35 +0700

    login fix

---
+++ angular-login-admin/src/app/auth/login.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../services';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './login.component.html',
+})
+export class LoginComponent {
+  username = '';
+  password = '';
+  error = '';
+
+  constructor(private auth: AuthService, private router: Router) {}
+
+  submit() {
+    this.error = '';
+    if (this.auth.login(this.username, this.password)) {
+      this.router.navigate(['/home']);
+    } else {
+      this.error = 'Username atau password salah';
+    }
+  }
+}
